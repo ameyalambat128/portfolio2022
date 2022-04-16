4d8bc820532179bd568ba9f150bed3a0f45171d1
Ameya Lambat <ameyalambat@gmail.com>
2022-04-16 01:19:51 -0700

    initial commit

---
+++ components/Masthead.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Masthead = () => {
+  return (
+    <div>
+      <div className="min-h-screen flex flex-col items-center justify-center">
+        <div className="p-12 font-bold z-10"></div>
+        Masthead
+      </div>
+    </div>
+  );
+};
+
+export default Masthead;
