bcda9c93eb272ea7c24e444db2a433f31b037d80
Ameya Lambat <ameyalambat@gmail.com>
2022-04-16 01:28:08 -0700

    fix: removed the masthead file and replaced it with and landing file. Some unknown issue was causeing the trouble

--- components/Landing.jsx
+++ components/Landing.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 
-const Masthead = () => {
+const Landing = () => {
   return (
     <div>
       <div className="min-h-screen flex flex-col items-center justify-center">
         <div className="p-12 font-bold z-10"></div>
-        Masthead
+        <h1 className="">Ameya Lambat</h1>
+        <h2 className="">
+          <span>Full Stack Developer</span>
+        </h2>
       </div>
     </div>
   );
 };
 
-export default Masthead;
+export default Landing;
