779de75c2080d733c534f86d8c95ceb4c1a8a035
Ameya Lambat <ameyalambat@gmail.com>
2022-05-07 01:15:22 -0700

    fix: fixed the work image alt tag

--- components/Work.jsx
+++ components/Work.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Work = ({ image, title, description, width, height }) => {
   return (
     <div className="flex flex-col items-center justify-start outline-1 bg-white border-0 rounded-lg p-5 shadow-lg">
-      <Image src={image} alt="appdev" width={1366 / 2.5} height={768 / 2.5} />
+      <Image src={image} alt="work" width={1366 / 2.5} height={768 / 2.5} />
       <h2 className="mt-5 text-2xl lg:text-3xl font-bold">{title}</h2>
       <p className="mt-5 text-sm md:text-lg font-medium">{description}</p>
     </div>
